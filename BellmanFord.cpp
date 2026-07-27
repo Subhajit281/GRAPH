3f8e0ec8b2226b7407b19b6a2d957958a703caac
Subhajit281 <subhajitsarkar281@gmail.com>
2026-07-27 16:52:22 +0530

    Bellman Ford

---
+++ BellmanFord.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        
+        // just relax all edges V-1 times 
+
+        vector<int>  dist(V,1e8);
+        dist[src] = 0;
+        for(int i = 0 ; i < V-1 ; i++){
+            for(auto& it : edges){
+                int u = it[0];
+                int v = it[1];
+                int wt = it[2];
+                if(dist[u] != 1e8 && dist[u] + wt < dist[v]){
+                    dist[v] = dist[u] + wt;
+                }
+            }
+        }
+        // Negative edge cycle check (Nth iteration)
+        for(auto& it:edges){
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            if(dist[u] != 1e8 && dist[u] + wt < dist[v]){
+                return {-1};
+            }
+        }
+        
+        
+        return dist;
+    }
+};
