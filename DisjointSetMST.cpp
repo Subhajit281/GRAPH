aefa4211e6c70124db1790a12f5ea0dfe47c143c
Subhajit281 <subhajitsarkar281@gmail.com>
2026-08-12 17:49:43 +0530

    DisjointSet & templateCpp

---
+++ DisjointSetMST.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    class DisjointSet{
+        vector<int> parent;
+        vector<int> rank;
+
+    public: 
+        DisjointSet(int n){
+            rank.resize(n,0);
+            for(int i = 0 ; i < n ; i++){
+                parent.push_back(i);
+            }
+        };
+
+        int findUParent(int node){
+            if(parent[node] == node) return node;
+            return parent[node] = findUParent(parent[node]);
+        }
+
+        void unionByRank(int u , int v){
+            int ulp_u = findUParent(u);
+            int ulp_v = findUParent(v);
+            if(ulp_u == ulp_v) return;
+            else if(rank[ulp_u] < rank[ulp_v]) parent[ulp_u] = ulp_v;
+            else if(rank[ulp_u] > rank[ulp_v]) parent[ulp_v] = ulp_u;
+            else{
+                parent[ulp_u] = ulp_v;
+                rank[ulp_v]++;
+            }
+        }
+    };
+    
+    // int makeConnected(int n, vector<vector<int>>& connections) {
+
+    //     DisjointSet ds(n);
+
+    //     int components = n;
+    //     int extra = 0;
+
+    //     for(auto connection : connections){
+    //         int u = connection[0];
+    //         int v = connection[1];
+    //         if(ds.findUParent(u) != ds.findUParent(v)){
+    //             ds.unionByRank(u,v);
+    //             components--;
+    //         }else extra++;
+    //     }
+
+    //     if(extra >= components - 1){
+    //         return components - 1;
+    //     }
+    //     return -1;
+
+    // }
+};
